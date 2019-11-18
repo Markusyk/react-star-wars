f08b08b5d57a42239dc988fbba8f9617ce838ced
MPRONIUK <markprnk@gmail.com>
2019-11-18 16:17:33 +0200

    Updates

---
+++ precache-manifest.0bd9a96a6d4fd674a2007b4525e8e357.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb6959fc86b15441a3e25f4903eada0",
+    "url": "/react-star-wars/index.html"
+  },
+  {
+    "revision": "fd070797976b9e0b70e5",
+    "url": "/react-star-wars/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "647091d9dcdf025ccf50",
+    "url": "/react-star-wars/static/js/2.d5b633b2.chunk.js"
+  },
+  {
+    "revision": "fd070797976b9e0b70e5",
+    "url": "/react-star-wars/static/js/main.0cd5f73d.chunk.js"
+  },
+  {
+    "revision": "3c421b460294fc89e4bc",
+    "url": "/react-star-wars/static/js/runtime-main.846a5c77.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-star-wars/static/media/logo.25bf045c.svg"
+  }
+]);
